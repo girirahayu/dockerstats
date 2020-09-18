858a0a94b257b087e95c741351a800e86a1e0744
grahayu <giri.rahayu@xapiens.id>
2020-09-18 08:22:59 +0700

    add install kubectl

---
+++ k8s/dep.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+
+mv kubectl /bin/
